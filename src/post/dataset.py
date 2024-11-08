04ec82978dc9d2332b3c8eae323614dfd1339e2a
Alexis Ballo <41343138+AlexisBallo2@users.noreply.github.com>
2024-11-07 21:59:46 -0500

    use draw not dolphin

--- src/post/dataset.py
+++ src/post/dataset.py
@@ -1,11 +1,13 @@
 
 from collections import Counter
 import json
-f = open("../../data/DRAW/dolphin_t2_final.json", encoding="utf-8")
-data = json.loads(f.read())
+f = open("../../data/DRAW/draw.json", encoding="utf-8")
+# f = open("../../data/DRAW/dolphin_t2_final.json", encoding="utf-8")
+dolphin = json.loads(f.read())
 
+print(len(dolphin))
 lengths = []
-for obs in data:
+for obs in dolphin:
     equations = obs['lSolutions']
     lengths.append(len(equations))
 
