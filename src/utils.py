fd6df4f8ce24f74d72efd39f25122ce7856b42f0
Alexis Ballo <41343138+AlexisBallo2@users.noreply.github.com>
2025-01-08 09:41:06 -0500

    fix act soln

--- src/utils.py
+++ src/utils.py
@@ -23,7 +23,7 @@ def solve_equation(equations, solutions):
             spEqs.append(sympy_eq)   
         solved = solve(spEqs, dict=True)
         # cur_targets = [round(i) for i in list(solved[0].values())]
-        act_solns = list(round(list(obj.values())[0]) for obj in solved)
+        act_solns = [round(i) for i in (list(solved[0].values()))]
 
         print("act", act_solns)
         print("pred", solutions)
