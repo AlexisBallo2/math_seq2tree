119fbb0522aaafb2ea6d8da6f588e065d3013618
Alexis Ballo <41343138+AlexisBallo2@users.noreply.github.com>
2025-01-08 15:19:29 -0500

    80 epochs, dont log epoch text

--- run_seq2tree.py
+++ run_seq2tree.py
@@ -81,10 +81,10 @@ else:
         "batch_size": 20,
         "embedding_size": 128,
         "hidden_size": 512,
-        # "n_epochs": 80,
+        "n_epochs": 80,
         # "n_epochs": 20,
         # "n_epochs": 10,
-        "n_epochs": 3,
+        # "n_epochs": 3,
         "learning_rate": 1e-3,
         "weight_decay": 1e-5,
         "beam_size": 5,
@@ -500,7 +500,7 @@ for fold in range(folds_to_do):
             batch_eval_num_x_mse = sum(batch_accuricies["eval_num_x_mse"]) / len(batch_accuricies["eval_num_x_mse"])
             batch_eval_sni_acc = sum(batch_accuricies["eval_sni_acc"]) / len(batch_accuricies["eval_sni_acc"])
             batch_eval_soln_acc = sum(batch_accuricies["eval_soln"]) / len(batch_accuricies["eval_soln"])
-            print('epoch', epoch, 'fold', fold, 'batch eval soln', batch_eval_soln_acc)
+            print(epoch, 'batch eval soln', batch_eval_soln_acc)
             eval_comparison.append(batch_eval_comparison)
 
             print("loss:", batch_loss)
