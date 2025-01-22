e2459b0708685ef228d4d3c27ddd9c39a2e554b9
Alexis Ballo <41343138+AlexisBallo2@users.noreply.github.com>
2025-01-22 10:23:54 -0500

    new eval

--- run_seq2tree.py
+++ run_seq2tree.py
@@ -633,7 +633,7 @@ for fold in range(existing_fold, folds_to_do):
                 for k, v in models.items():
                     v.eval()
                 input_batch_len = len(input_batches[idx])
-                solved = evaluate_tree( input_batches[idx], input_lengths[idx], output_batches[idx], output_lengths[idx], num_stack_batches[idx], nums_batches[idx], num_size_batches[idx], output_var_batches[idx], generate_num_ids, models, output_lang, num_pos_batches[idx], equation_targets[idx], var_pos[idx], batches_sni[idx], pair_mapping[idx],output_var_solutions[idx], config['useCustom'], vars, config['setName'], config['useSemanticAlignment'], config['useSeperateVars'], config['useOpScaling'], config['useSNIMask'], config['useTFix'], datasets[idx], beam_size, False) 
+                solved = evaluate_tree( input_batches[idx], input_lengths[idx], output_batches[idx], output_lengths[idx], num_stack_batches[idx], num_actual_batches[idx], num_size_batches[idx], output_var_batches[idx], generate_num_ids, models, output_lang, num_pos_batches[idx], equation_targets[idx], var_pos[idx], batches_sni[idx], pair_mapping[idx],output_var_solutions[idx], config['useCustom'], vars, config['setName'], config['useSemanticAlignment'], config['useSeperateVars'], config['useOpScaling'], config['useSNIMask'], config['useTFix'], datasets[idx], beam_size, False) 
                 # test_time_array.append([input_batch_len,end - start])
                 # testc.append(comparison)
                 # if idx > 2:
